d5e560358076ed5436cef46081d43df5d2ff3971
jmarkow <jmarkow@cns.bu.edu>
2017-03-21 15:06:03 -0400

    Bugfix to read options

--- @mat2tdt/get_device_number.m
+++ @mat2tdt/get_device_number.m
@@ -8,7 +8,7 @@ table={'pa5',33,'rp2',35,'rl2',36,...
 	'ra16',37,'rv8',39,'rx5',45,'rx6',46,...
 	'rx7',47,'rx8',48,'rz2',50,'rz5',53,'rz6',54};
 
-hit=find(strcmp(table,lower(ID)));
+hit=find(strcmp(table,cellstr(lower(ID))));
 
 if ~isempty(hit)
 	NUM=table{hit+1};
