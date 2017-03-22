d725f8d641296875d3e3d4ad3a8f3336f6eee1a8
jmarkow <jmarkow@cns.bu.edu>
2017-03-22 09:09:11 -0400

    Bugfix

--- @mat2tdt/connect_device.m
+++ @mat2tdt/connect_device.m
@@ -10,6 +10,8 @@ end
 OBJ.settings.zbus_address=OBJ.activex.zbus.GetDeviceAddr(OBJ.get_device_number(OBJ.settings.dev),1);
 OBJ.settings.zbus_rack_num=ceil((OBJ.settings.zbus_address-1)/2);
 
+fprintf('Connecting to device...');
+
 switch lower(OBJ.settings.dev)
 case 'rx8'
 	connect_status=OBJ.activex.dev.ConnectRX8(upper(OBJ.settings.interface),1);
@@ -17,8 +19,10 @@ otherwise
 	fprintf('Device %s currently not supported\n',OBJ.settings.dev);
 end
 
-if connect_status==0
-	error('Error connecting to %s',OBJ.settings.dev)
+if connect_status==1
+	fprintf('success\n');
+else
+	fprintf('failed\n')
 end
 
 OBJ.update_status;
--- @mat2tdt/update_tag.m
+++ @mat2tdt/update_tag.m
@@ -14,7 +14,7 @@ end
 
 OBJ.collect_tags;
 SUCCEED=false;
-
+EPS=1e-6;
 if isfield(OBJ.tags,TAG)
 
   tag_class=class(OBJ.tags.(TAG));
@@ -37,15 +37,16 @@ if isfield(OBJ.tags,TAG)
 
     newval=OBJ.activex.dev.GetTagVal(TAG);
 
-    if newval==VAL & SUCCEED
-      fprintf('succeeded\n');
-    else
-      if isnumeric(newval)
-        fprintf('failed, new value %g\n',newval)
+    if isnumeric(newval)
+      if abs(newval-VAL)<=EPS & SUCCEED
+        fprintf('succeeded\n');
       else
-        fprintf('failed, new value %s\n',newval);
+        fprintf('failed, current value %g\n',newval)
       end
+    elseif ~SUCCEED
+        fprintf('failed, current value %s\n',newval);
     end
+
   else
     fprintf('Skipping %s, used class %s but correct class is %s\n',...
       TAG,tag_class,use_class);
