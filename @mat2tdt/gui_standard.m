864a09d73e472bc8bfb193bc98da589f57aee14c
jmarkow <jmarkow@cns.bu.edu>
2017-03-22 09:37:40 -0400

    Gui work

---
+++ @mat2tdt/gui_standard.m
@@ -0,0 +1,76 @@
+function gui_standard(OBJ)
+%
+%
+%
+
+
+if ~OBJ.status.circuit_loaded
+	fprintf('Need to load a circuit first...\n');
+	return;
+end
+
+tag_names=fieldnames(OBJ.tags);
+ntags=length(tag_names);
+
+fontsize=10;
+top_pos=.95;
+bottom_pos=.3;
+ncols=floor(ceil/OBJ.settings.gui_rows_per_column);
+
+column_width=(OBJ.settings.gui_width-50)/ncols;
+px_space=top_pos-bottom_pos;
+vert_space_tot=vert_space*ntags;
+
+rem_space=px_space-vert_space;
+box_height=rem_space/ntags;
+
+tdt_figure=figure('Visible','off','Name',['TDT interface v.001a'],...
+	'Position',[50,50,OBJ.settings.gui_width,min(gui_row_height*ntags,gui_height)],...
+	'NumberTitle','off',...
+	'menubar','none','resize','on');
+
+tag_axis=struct();
+name_axis=struct();
+
+cur_pos=top_pos;
+
+column_width=max(.05,(1/ncols-.05));
+text_width=column_width*.75;
+edit_width=column_width-text_width;
+
+for i=1:ntags
+
+	% start at top
+	% callback is to reset the tag if we edit the text
+
+	cur_column=floor(rows_per_column/i);
+	left_edge=cur_column*column_width+.05;
+
+	name_axis.(tag_names{i})=uicontrol(tdt_figure,'Style','text',...
+		'String',tag_names{i},...
+		'Units','Normalized',...
+		'FontSize',fontsize,...
+		'Position',[left_edge cur_pos-box_height text_width box_height/2]);
+
+	pos=get(name_axis(i),'position');
+
+	tag_axis.(tag_names{i})=uicontrol(tdt_figure,'Style','edit',...
+		'String',OBJ.tags.tag_names{i},...
+		'Units','Normalized',...
+		'FontSize',fontsize,...
+		'Position',[pos(1)+pos(3)+.05 cur_pos-box_height edit_width box_height]);
+		%'Call',{@mat2tdt_set_tag,dev,tags(i).name});
+
+	cur_pos=cur_pos-box_height-vert_space;
+
+end
+
+tag_axis.update_tags=uicontrol(tdt_figure,'Style','PushButton',...
+	'String',sprintf('Push tags to %s',OBJ.settings.dev),...
+	'Units','Normalized',...
+	'FontSize',fontsize,...
+	'Position',[.05 .25 .3 .1]);
+
+% add buttons for:  (1) updating tags (2) doing buffer stuff
+
+set(tdt_figure,'visible','on');
