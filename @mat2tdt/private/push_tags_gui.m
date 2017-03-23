b1256e03ee94a52098379f4a2f16a1739c49ad53
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2017-03-23 09:35:31 -0400

    Forgot what I did

--- @mat2tdt/private/push_tags_gui.m
+++ @mat2tdt/private/push_tags_gui.m
@@ -10,7 +10,7 @@ tag_names_set=fieldnames(OBJ.tags);
 
 for i=1:length(tag_names_gui)
 	if isfield(OBJ.tags,tag_names_gui{i})
-		tmp_val=str2num(get(TAG_HANDLES.tag_names_gui{i},'String'));
+		tmp_val=str2num(get(TAG_HANDLES.(tag_names_gui{i}),'String'));
 		if abs(OBJ.tags.(tag_names_gui{i})-tmp_val)>=EPS
 			OBJ.update_tag(tag_names_gui{i},tmp_val);
 		end
